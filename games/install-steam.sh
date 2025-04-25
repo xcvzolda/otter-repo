64e279352b7a4f9601a0aeb5796f63bc17d94492
xcvzolda <166529366+xcvzolda@users.noreply.github.com>
2025-04-25 18:28:24 +0300

    Delete games/install-steam.sh

--- games/install-steam.sh
+++
@@ -1,5 +0,0 @@
-echo Fetching steam
-wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
-echo Installing .deb
-sudo dpkg -i steam.deb
-echo Done!. Look for steam in your start menu (Or how you call it) Or just run "steam".
